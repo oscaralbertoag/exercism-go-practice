96e8f23c04fe30b38809da7ee1f14a9a61622260
oscaralbertoag <oaahh4@mst.edu>
2020-02-11 12:11:26 -0700

    Solve Hamming Difference problem.

--- hamming/hamming.go
+++ hamming/hamming.go
@@ -1,4 +1,20 @@
 package hamming
 
+import (
+	"fmt"
+)
+
 func Distance(a, b string) (int, error) {
+	if len(a) != len(b) {
+		return 0, fmt.Errorf("Can't determine Hamming Distance for sequences of different length (a:%d, b:%d)!", len(a), len(b))
+	}
+
+	difference := 0
+	for indexA, value := range a {
+		if value != rune(b[indexA]) {
+			difference++
+		}
+	}
+
+	return difference, nil
 }
