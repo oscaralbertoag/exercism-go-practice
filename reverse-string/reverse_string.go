1a3349005f0bbc69d0224571a122224a7e637c8e
oscaralbertoag <oaahh4@mst.edu>
2020-02-12 10:07:08 -0700

    Solve reverse-string problem.

--- reverse-string/reverse_string.go
+++ reverse-string/reverse_string.go
@@ -1,1 +1,14 @@
 package reverse
+
+func Reverse(input string) string {
+	result := []rune(input)
+
+	length := len(result)
+	for i := 0; i < length/2; i++ {
+		temp := result[i]
+		result[i] = result[length-1-i]
+		result[length-1-i] = temp
+	}
+
+	return string(result)
+}
